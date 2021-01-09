b8c76b543b3c6089f81a56021286e827017a1f8b
Oguzcan Adabuk <adabuk2@hotmail.com>
2021-01-08 22:25:48 -0600

    first commit

---
+++ MeshGeometry/Assets/_Scripts/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿/* Copyrighted by Oguzcan Adabuk Chicago, IL, 2018*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 50f;
+
+    public Transform camParent;
+    public Transform camTransfrom;
+
+    void Start () {
+		
+	}
+    
+    void Update () {
+
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Mouse ScrollWheel") * moveSpeed * 30 * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.position += moveX * camTransfrom.right;
+        transform.position += moveY * camTransfrom.up;
+        transform.position += moveZ * camTransfrom.forward;
+
+        float rotateY = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        float rotateX = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+
+        camTransfrom.Rotate(-rotateX, 0, 0);
+        camParent.Rotate(0, rotateY, 0);
+	}
+}
---
+++ MeshGeometry/Assets/_Scripts/MapBuilder.cs
@@ -0,0 +1,96 @@
+﻿/* Copyrighted by Oguzcan Adabuk Chicago, IL, 2018*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour {
+
+    public Texture2D heightmap;
+    public Material marsMaterial;
+    public int edge = 256;
+    public float heightScale = 20f;
+    public float widthScale = 10f;
+    
+    Color black = new Color(0, 0, 0);
+
+    void Start () {
+        for(int i = 0; i < heightmap.height; i += edge)
+        {
+            for (int j = 0; j < heightmap.width; j += edge)
+            {
+                BuildMapPart(i, j, edge);
+            }
+        }
+    }
+
+    public void BuildMapPart(int x, int y, int e)
+    {
+        GameObject mapPart = new GameObject();
+        mapPart.AddComponent<MeshFilter>();
+        mapPart.AddComponent<MeshRenderer>();
+        mapPart.name = "Map_Part_" + x + "_" + y;
+
+        Color[] mapPix = heightmap.GetPixels(x, y, e, e);
+
+        List<Vector3> vertexPositions = new List<Vector3>();
+        List<Vector2> uvPositions = new List<Vector2>();
+        List<Vector3> normalPositions = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        int mapSize = e * e;
+        int n = 0;
+
+        for (int i = 0; i < e; i++)
+        {
+            for (int j = 0; j < e; j++)
+            {
+                Vector3 pos = new Vector3(
+                    (y + i ) * widthScale,
+                    GetHeight(mapPix[n]) * heightScale,
+                    (x + j) * widthScale
+                );
+                vertexPositions.Add(pos);
+                uvPositions.Add(new Vector2(i, j));
+                normalPositions.Add(new Vector3(5, 0, 0));
+                n++;
+            }
+        }
+
+        for (int k = 0; k + e + 1 < mapSize; k++)
+        {
+            if((k + 1) % e != 0)
+            {
+                triangles.Add(k);
+                triangles.Add(k + 1);
+                triangles.Add(k + 1 + e);
+
+                triangles.Add(k + 1 + e);
+                triangles.Add(k + e);
+                triangles.Add(k);
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        MeshFilter mf = mapPart.GetComponent<MeshFilter>();
+        MeshRenderer mr = mapPart.GetComponent<MeshRenderer>();
+
+        mesh.vertices = vertexPositions.ToArray();
+        mesh.uv = uvPositions.ToArray();
+        mesh.normals = normalPositions.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+
+        mf.mesh = mesh;
+        mr.material = marsMaterial;
+    }
+    
+    public float GetHeight(Color c)
+    {
+        float distance = 
+            Mathf.Abs(c.r - black.r) +
+            Mathf.Abs(c.g - black.g) +
+            Mathf.Abs(c.b - black.b);
+        return distance;
+    }  
+}
+
